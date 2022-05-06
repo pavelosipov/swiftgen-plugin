c90675e92e695fe028a1c906413d051b566c9e41
Pavel Osipov <p.osipov@corp.mail.ru>
2022-05-06 14:04:26 +0300

    Attempt to find workaround for Xcode 100% CPU

--- Package.swift
+++ Package.swift
@@ -23,15 +23,15 @@ let package = Package(
       ),
       dependencies: ["rswift"]
     ),
-    // .binaryTarget(
-    //   name: "swiftgen",
-    //   url: "https://localhost:4433/swiftgen-v6.5.1.zip",
-    //   checksum: "1cde240d8a1cb7a3656329991fcd4f16113171dde45024cc2ade37d412bca876"
-    // ),
     .binaryTarget(
-      name: "rswift",
-      url: "https://localhost:4433/rswift-v6.1.0.zip",
-      checksum: "8a61de2954038023863b5eed2853e2bde8e6d7f73baa96b62334fa887a8690ae"
-    )
+      name: "swiftgen",
+      url: "https://localhost:4433/swiftgen-6.5.1.zip",
+      checksum: "8390e715f3c12504429e46478d42e68fb611a66154be6d881b5f6e878400e1aa"
+    ),
+    // .binaryTarget(
+    //   name: "rswift",
+    //   url: "https://localhost:4433/rswift-v6.1.0.zip",
+    //   checksum: "8a61de2954038023863b5eed2853e2bde8e6d7f73baa96b62334fa887a8690ae"
+    // )
   ]
 )
